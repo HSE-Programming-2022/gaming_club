4fb9af279f91c429ba95fbe22bf28b12edf00b9f
BuzzErR <new.mikhailforwork@gmail.com>
2022-05-21 20:00:00 +0300

    vue js 2.0

---
+++ frontend/src/views/ContactsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>
+      Контакты
+    </h1>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
+
+<script>
+export default {
+  name: 'ContactsView',
+}
+</script>
