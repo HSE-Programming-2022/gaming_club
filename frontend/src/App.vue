80fb74e54296cc12e5022561d0aaee127b0acd33
Lilia <lilia@The-biggest.local>
2022-05-23 17:59:21 +0300

    Prices page (wip) added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,7 +4,7 @@
         id="scrolling-techniques-6"
         class="overflow-y-auto"
     >
-        <v-content style="height: 100vh;">
+        <v-content>
           <router-view>
           </router-view>
         </v-content>
@@ -21,6 +21,12 @@
   color: #2c3e50;
   background-color: aqua;
 }
+
+@font-face {
+    font-family: "our_font";
+    src: local("our_font"),
+     url("@/assets/9331.ttf") format("truetype");
+  }
 </style>
 <script>
 
