bdc4906e9ed294ab0ea8a7a8e2512934eec6b0c7
BuzzErR <new.mikhailforwork@gmail.com>
2022-06-14 00:26:25 +0300

    docker compose added and user check fixed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-  <v-app>
+    <v-app>
       <div v-if="this.loadedUserInfo">
         <NavigationBar :userLoggedInProp=this.userLoggedIn>
         </NavigationBar>
@@ -14,118 +14,118 @@
         </v-sheet>
       </div>
       <div v-else class="loading">
-<!--      <h1>Готовимся к вашему приходу...</h1>-->
-<!--      <div class='pac-man' />-->
+        <!--      <h1>Готовимся к вашему приходу...</h1>-->
+        <!--      <div class='pac-man' />-->
         <div class="logo"><b>R<span>E</span>B<span>O</span><span>R</span>N</b></div>
-    </div>
-  </v-app>
+      </div>
+    </v-app>
   </div>
 </template>
 
 <style lang="scss">
-  @import url(//fonts.googleapis.com/css?family=Vibur);
-  @import './assets/pacman.scss';
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-  }
+@import url(//fonts.googleapis.com/css?family=Vibur);
+@import './assets/pacman.scss';
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
 
-  @font-face {
-      font-family: "our_font";
-      src: local("our_font"),
-       url("@/assets/9331.ttf") format("truetype");
-    }
+@font-face {
+  font-family: "our_font";
+  src: local("our_font"),
+  url("@/assets/9331.ttf") format("truetype");
+}
 
-  .loading {
-    height:100%;
-    background: #112 url("@/assets/loadingbackground.gif") center no-repeat;
-    background-size: auto;
-  }
-  .logo {
-    text-align: center;
-    width: 65%;
-    height: 250px;
-    margin: auto;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 22em;
-    user-select: none;
-  }
+.loading {
+  height:100%;
+  background: #112 url("@/assets/loadingbackground.gif") center no-repeat;
+  background-size: auto;
+}
+.logo {
+  text-align: center;
+  width: 65%;
+  height: 250px;
+  margin: auto;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 22em;
+  user-select: none;
+}
 
-  .logo b{
-    font: 400 7vh "our_font";
-    color: #fee;
-    text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000;
-    margin-right: 7em;
+.logo b{
+  font: 400 7vh "our_font";
+  color: #fee;
+  text-shadow: 0 -40px 100px, 0 0 2px, 0 0 1em #ff4444, 0 0 0.5em #ff4444, 0 0 0.1em #ff4444, 0 10px 3px #000;
+  margin-right: 7em;
+}
+.logo b span{
+  animation: blink linear infinite 2s;
+}
+.logo b span:nth-of-type(2){
+  animation: blink linear infinite 3s;
+}
+@keyframes blink {
+  78% {
+    color: inherit;
+    text-shadow: inherit;
   }
-  .logo b span{
-    animation: blink linear infinite 2s;
+  79%{
+    color: #333;
   }
-  .logo b span:nth-of-type(2){
-    animation: blink linear infinite 3s;
-  }
-  @keyframes blink {
-    78% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    79%{
-      color: #333;
-    }
-    80% {
+  80% {
 
-      text-shadow: none;
-    }
-    81% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    82% {
-      color: #333;
-      text-shadow: none;
-    }
-    83% {
-      color: inherit;
-      text-shadow: inherit;
-    }
-    92% {
-      color: #333;
-      text-shadow: none;
-    }
-    92.5% {
-      color: inherit;
-      text-shadow: inherit;
-    }
+    text-shadow: none;
   }
+  81% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+  82% {
+    color: #333;
+    text-shadow: none;
+  }
+  83% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+  92% {
+    color: #333;
+    text-shadow: none;
+  }
+  92.5% {
+    color: inherit;
+    text-shadow: inherit;
+  }
+}
 
 
-  /* follow me @nodws */
-  #btn-twtr{
-    clear:both;
-    color:#fff;
-    border:2px solid;
-    border-radius:3px;
-    text-align:center;
-    text-decoration:none;
-    display:block;
-    font-family:sans-serif;
-    font-size:14px;
-    width:13em;
-    padding:5px 10px;
-    font-weight:600;
-    position:absolute;
-    bottom:20px;
-    left:0;
-    right:0;
-    margin:0 auto;
-    background:rgba(0,0,0,0.2);
-    box-shadow:0 0 0px 3px rgba(0,0,0,0.2);
-    opacity:0.4
-  }#btn-twtr:hover{color:#fff;opacity:1}
+/* follow me @nodws */
+#btn-twtr{
+  clear:both;
+  color:#fff;
+  border:2px solid;
+  border-radius:3px;
+  text-align:center;
+  text-decoration:none;
+  display:block;
+  font-family:sans-serif;
+  font-size:14px;
+  width:13em;
+  padding:5px 10px;
+  font-weight:600;
+  position:absolute;
+  bottom:20px;
+  left:0;
+  right:0;
+  margin:0 auto;
+  background:rgba(0,0,0,0.2);
+  box-shadow:0 0 0px 3px rgba(0,0,0,0.2);
+  opacity:0.4
+}#btn-twtr:hover{color:#fff;opacity:1}
 </style>
 <script>
 
@@ -158,31 +158,39 @@ export default {
     }
   },
   methods: {
-      async preload_data(){
-        if (!this.loadedUserInfo) {
-          let user = localStorage.getItem('user-info')
-          if (user)
-          {
-            let userJson = JSON.parse(user)
-            let result = await axios.post("http://127.0.0.1:8080/users/verify", {
-              email: userJson.email,
-              password: userJson.password,
-            });
-            if (result.status === 200) {
-              this.userLoggedIn = true
-            }
-            else
-            {
-              localStorage.removeItem('user-info')
-              this.userLoggedIn = false
-            }
+    async preload_data(){
+      if (!this.loadedUserInfo) {
+        let user = localStorage.getItem('user-info')
+        let user_found = true
+        if (user)
+        {
+          let userJson = JSON.parse(user)
+          await axios.post("http://127.0.0.1:8080/users/verify", {
+            email: userJson.email,
+            password: userJson.password,
+          })
+              .then(function (response) {
+
+                if (response.status === 200) {
+                  this.userLoggedIn = true
+                }
+              })
+              .catch(function (error) {
+                if (error.response) {
+                  user_found = false
+                }
+              });
+          if (!user_found) {
+            localStorage.removeItem('user-info')
+            this.userLoggedIn = false
           }
         }
-        this.loadedUserInfo = true
       }
+      this.loadedUserInfo = true
+    }
   },
   mounted() {
-      this.preload_data();
-    },
+    this.preload_data();
+  },
 }
 </script>
